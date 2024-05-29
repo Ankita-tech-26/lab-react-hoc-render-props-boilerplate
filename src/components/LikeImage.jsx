1590a0cd3aa63a866a5ef216d7cc55373cb6acb3
Ankita Laxmi <74234178+Ankita-tech-26@users.noreply.github.com>
2024-05-29 16:52:16 +0530

    Added

--- src/components/LikeImage.jsx
+++ src/components/LikeImage.jsx
@@ -1,19 +1,4 @@
-// import React, { useState } from 'react'
 
-// export default function LikeImage() {
-
-//   const [likeImageCounter, setLikeImageCounter] = useState(0);
-
-//   const handleLikeImageCount = ()=>{
-//     setLikeImageCounter(likeImageCounter+1);
-//   }
-
-//   return (
-//     <div>
-//       <button onClick={handleLikeImageCount}>Like Image {likeImageCounter}</button>
-//     </div>
-//   )
-// }
 import React from 'react';
 import LikeFunctionality from './LikeFunctionality';
 
--- src/components/LikePost.jsx
+++ src/components/LikePost.jsx
@@ -1,19 +1,4 @@
-// import React, { useState } from 'react'
 
-// export default function LikePost() {
-
-//   const [likePostCounter, setPostCounter] = useState(0);
-
-//   const handlePostCount = ()=>{
-//     setPostCounter(likePostCounter+1);
-//   }
-
-//   return (
-//     <div>
-//       <button onClick={handlePostCount}>Like Post {likePostCounter}</button>
-//     </div>
-//   )
-// }
 
 import React from 'react';
 import LikeFunctionality from './LikeFunctionality';
